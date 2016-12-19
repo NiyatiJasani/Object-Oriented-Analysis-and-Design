e9befeec95f84226fd10b8a29d0d21c41c66e8a1
NiyatiJasani <niyatijasani6@gmail.com>
2016-12-19 09:14:46 +0530

    Add files via upload

---
+++ boardgame/Tile.java
@@ -0,0 +1,33 @@
+package com.techlabs.boardgame;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Tile {
+	 private List units;
+	    
+	    public Tile()
+	    {
+	        units = new LinkedList();
+	    }
+	    
+	    protected void addUnit(Unit unit)
+	    {
+	        units.add(unit);
+	    }
+	    
+	    protected void removeUnit(Unit unit)
+	    {
+	        units.remove(unit);
+	    }
+	    
+	    protected void removeUnits()
+	    {
+	        
+	    }
+	    
+	    protected List getUnits()
+	    {
+	        return units;
+	    }
+}
---
+++ boardgame/Unit.java
@@ -0,0 +1,12 @@
+package com.techlabs.boardgame;
+
+import java.awt.List;
+import java.util.LinkedList;
+
+public class Unit {
+
+	public Unit() {
+
+	}
+
+}
