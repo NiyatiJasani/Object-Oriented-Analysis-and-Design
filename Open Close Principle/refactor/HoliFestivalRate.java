3ad53c2bb87b758420d19742f214bd7dbbf75fa1
NiyatiJasani <niyatijasani6@gmail.com>
2016-12-26 10:21:24 +0530

    Open Close Principle

---
+++ Open Close Principle/refactor/HoliFestivalRate.java
@@ -0,0 +1,11 @@
+package ocp.refactor;
+
+public class HoliFestivalRate implements IFestivalRate {
+
+	@Override
+	public double getRate() {
+		// TODO Auto-generated method stub
+		return 0.8;
+	}
+
+}
---
+++ Open Close Principle/refactor/RamzanFestivalRate.java
@@ -0,0 +1,11 @@
+package ocp.refactor;
+
+public class RamzanFestivalRate implements IFestivalRate{
+
+	@Override
+	public double getRate() {
+		// TODO Auto-generated method stub
+		return 0.6;
+	}
+
+}
