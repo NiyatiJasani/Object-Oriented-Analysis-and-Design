9a86e87c649b9d7b956c1539170daae313d60969
NiyatiJasani <niyatijasani6@gmail.com>
2016-12-26 10:28:37 +0530

    Open Close Principle (OCP)

---
+++ Open Close Principle/violaationtest/TestFixedDeposit.java
@@ -0,0 +1,17 @@
+package ocp.violaationtest;
+
+import ocp.violation.FestivalType;
+import ocp.violation.FixedDeposit;
+
+//public class TestFixedDeposit {
+	//public static void main(String[] args) {
+	//	FixedDeposit holder1 = new FixedDeposit("177h", 100000, , FestivalType.HOLI);
+	
+
+	
+		
+		
+		//System.out.println(holder1.calculateRateOfInterest());
+	//}
+
+//}
