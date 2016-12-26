6db2d71a804a3c59b6905acd9762aa1f2aa6a829
NiyatiJasani <niyatijasani6@gmail.com>
2016-12-26 10:02:30 +0530

    Add files via upload

---
+++ Open Close Principle/refactor/NormalFestivalRate.java
@@ -0,0 +1,11 @@
+package ocp.refactor;
+
+public class NormalFestivalRate implements IFestivalRate{
+
+	@Override
+	public double getRate() {
+		// TODO Auto-generated method stub
+		return 0.5;
+	}
+
+}
